5a8f9fe2398ad961b8efda983549e332b3e9ceb7
okomojacob <jaylabs2030@gmail.com>
2021-03-07 21:55:42 +0300

    new file, 2 sum

--- leetCode/Easy/two-sum.py
+++ leetCode/Easy/two-sum.py
@@ -1,6 +1,6 @@
 """
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-You assume that each input would have exactly one solution
+Given an array of integers(nums), return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution
 
 Maintain a mapping from from each number to it's index
 Check if targetr - num has already been found
