eda56a34924b3f7b109245253657894907bcfed0
okomojacob <jaylabs2030@gmail.com>
2021-03-10 22:19:11 +0300

    new file, 2 sum

--- leetCode/Easy/two-sum.py
+++ leetCode/Easy/two-sum.py
@@ -6,6 +6,12 @@ Maintain a mapping from from each number to it's index
 Check if targetr - num has already been found
 Time -0(n)
 Space - 0(n) for the dictionary
+EXAMPLE:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 """
 from typing import List
 class Solution(object):
