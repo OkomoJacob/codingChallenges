3c25677435b0f7e514a0446468ff9495c63bb14e
Jacob Okello Okomo <jaylabs2030@gmail.com>
2021-09-16 17:38:38 +0300

    :tada:, :hammer:

---
+++ 1.Data Structures and Algorithms; leetCode/Python/Medium/mergeIntervals.py
@@ -0,0 +1,11 @@
+'''Given a collection of intervals, merge all overlapping intervals
+Example 1: 
+    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+    Output: [[1,6],[8,10],[15,18]]
+    Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6]
+
+Exampl 2:
+    Input: intervals = [[1,4],[4,5]]
+    Output: [1,5]
+    Explanation: Since intervals [1,4] and [4,5] overlaps, merge them into [1,5]
+'''
