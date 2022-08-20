b9e5cd4d576dc9a60a57270621176dc42d4ad65d
okomojacob <jaylabs2030@gmail.com>
2022-08-20 12:34:48 +0300

    Folder cleaning :hammer:

--- 1.DataStructuresAlgorithms/1.JavaScript/Easy/17_palindrome.js
+++
@@ -1,6 +0,0 @@
-/**
- * Write a function to determine if the supplied string is paalindrome:
- * E.g: radar, mum, kayak, redder are all palindromes 
- * They are spelled same backwards and forwards 
- */
-
